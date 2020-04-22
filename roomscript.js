ba869abc1994eae18cb05e3d7dae4d5179f79a36
sarahrheerhee <36961253+sarahrheerhee@users.noreply.github.com>
2020-04-22 08:16:50 -0500

    Add files via upload

--- roomscript.js
+++ roomscript.js
@@ -3,7 +3,7 @@ $(window).load(function () {
         setTimeout(function(){
             $('.loader-wrapper').fadeOut('slow', function () {
             });
-        },000); // set the time here
+        },2000); // set the time here
 });
    $(function(){
    setTimeout(function(){
